6d4793de2e5cb44a13ceb0338d4ad3daf0b659d5
viiem <viitanenem@gmail.com>
2026-03-30 13:09:15 +0300

    //adding function placeholders

--- script.js
+++ script.js
@@ -8,17 +8,38 @@ window.onload = function() {
     tasks.forEach(task => addTaskToDOM(task.text, task.completed));
 };
 addBtn.addEventListener('click', () => {
-    const taskText = taskInput.value;
+    const taskText = taskInput.value.trim();
     if (taskText === "") return;
 
-    const li = document.createElement('li');
-    li.textContent = taskText;
+    addTaskToDOM(taskText, false);
+    saveTask(taskText, false);
 
-    // to delete task
-    li.addEventListener('click', () => {
-        taskList.removeChild(li);
-    });
+    taskInput.value = "";
 
     taskList.appendChild(li);
     taskInput.value = "";
 });
+//Add to DOM
+function addTaskToDOM(text,completed){
+    const li = document.createElement('li)');
+    li.textContent = text;
+    if (completed) li.classList.add('completed');
+
+  /*  // to delete task
+    li.addEventListener('click', () => {
+        li.classList.toggle('completed');
+        updateTaskInStorage(li.textContent, li.classList.contains('completed'));
+    });
+    taskList.appendChild(li);
+*/
+}
+//save task to localstorage
+function saveTask(text,completed){
+    //tasks
+    //tasks.push
+}
+//update task in localstorage
+function updateTaskInStorage(text, completed){
+    const tasks
+    const task
+}
